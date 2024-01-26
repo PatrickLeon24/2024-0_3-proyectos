b153e4ed4674fcaf15c9bdbdd5c2440653a9877d
PatrickLeon24 <20211454@ALOE.ULIMA.EDU.PE>
2024-01-26 11:31:48 -0500

    c

--- src/presentation/login/LoginPage.jsx
+++ src/presentation/login/LoginPage.jsx
@@ -1,6 +1,45 @@
 import { Box, Button, Container, TextField } from "@mui/material"
+import {useState} from "react"
+import dataUsuarios from "../../data/usuarios"
+import * as React from 'react';
+import Alert from '@mui/material/Alert';
+import CheckIcon from '@mui/icons-material/Check';
+import { useNavigate } from "react-router-dom";
+
 
 const LoginPage = () => {
+
+    const [username,setUsername]=useState("")
+    const [password,setPassword]=useState("")
+    const [loginIncorrecto,setLoginIncorrecto]=useState(false)
+    //navegacion programatica 
+    const navigate =useNavigate()
+
+
+    const usernameOnChangeHandler=(event)=>{
+        setUsername(event.target.value)
+    }
+    const passwordOnChangeHandler=(event)=>{
+        setPassword(event.target.value)
+    }
+
+    const loginOnClick=()=>{
+        const listaFiltrada=dataUsuarios.filter((usuario)=>{
+            return usuario.username== username && usuario.password ==password
+
+        })
+        if (listaFiltrada.length>0) {
+            console.log("correcto")
+            navigate("/main")
+        }
+        else{
+            console.log("incorrecto")
+            setLoginIncorrecto(true)
+            console.log(loginIncorrecto)
+        }
+        
+        
+    }
     return <Container maxWidth="sm">
         <Box component="form"
             noValidate
@@ -11,18 +50,43 @@ const LoginPage = () => {
             <div>
                 <TextField required
                     label="Username"
-                    margin="normal"/>
+                    margin="normal"
+                    value={username}
+                    onChange={usernameOnChangeHandler}/>
             </div>
             <div>
                 <TextField required
                     type="password"
                     label="Password"
-                    margin="normal"/>
+                    margin="normal"
+                    value={password}
+                    onChange={passwordOnChangeHandler}/>
             </div>
             <div>
-                <Button variant="contained" style={ { marginRight : "8px" }}>Login</Button>
+                <Button variant="contained" 
+                style={ { marginRight : "8px" }}
+                onClick={loginOnClick}>
+                Login
+                </Button>
                 <Button variant="contained">Registro</Button>
+                
             </div>
+            {
+                
+
+                
+                loginIncorrecto?
+                    <Alert icon={<CheckIcon fontSize="inherit" />} severity="error"
+                    sx={{mt:2}}
+                    >
+                        Here is a gentle confirmation that your action was successful.
+                    </Alert>
+                :""
+                
+                
+            }
+            
+            
 
         </Box>
     </Container>
